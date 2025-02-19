1d644cbe41742dd766d5e2e83bebe16b7b4409da
liam cawley <francisxaviercawley@gmail.com>
2025-02-18 23:35:52 -0500

    pathprefix: .git/COMMIT_EDITMSG

--- example/config.js
+++ example/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //pathPrefix: '/gatsby-theme-academic',
+  pathPrefix: '/site',
   siteUrl: 'https://www.cawley.dev',
   title: 'Liam Cawley',
   description: 'Personal Website of Liam Cawley',
